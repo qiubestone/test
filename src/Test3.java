bf7644a4d4494cddaaaa85754a58f9ea4b6de616
Aaron Wang <aaron.wang@hrx.ai>
2018-06-21 13:56:28 +0800

    psvm

--- src/Test3.java
+++ src/Test3.java
@@ -4,4 +4,9 @@
  * @author   <a href="mailto:aaron.wang@hrx.ai">Aaron Wang</a>
  * @version  06/21/2018 13:39
  */
-public class Test3 { }
+public class Test3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
