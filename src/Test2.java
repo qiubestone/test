18599cfae769658e35843605ce5a752800782a24
Aaron Wang <aaron.wang@hrx.ai>
2018-06-21 13:39:29 +0800

    Commit 2

--- src/Test2.java
+++ src/Test2.java
@@ -2,6 +2,6 @@
  * TODO: DOCUMENT ME!
  *
  * @author   <a href="mailto:aaron.wang@hrx.ai">Aaron Wang</a>
- * @version  06/21/2018 13:38
+ * @version  06/21/2018 13:39
  */
-public class Test1 { }
+public class Test2 { }
