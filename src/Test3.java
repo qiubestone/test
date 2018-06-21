0c7c318719755f82409ec52f96507ee96a82e71c
Aaron Wang <aaron.wang@hrx.ai>
2018-06-21 13:57:38 +0800

    Test2

--- src/Test3.java
+++ src/Test3.java
@@ -5,8 +5,14 @@
  * @version  06/21/2018 13:39
  */
 public class Test3 {
+  //~ Methods ----------------------------------------------------------------------------------------------------------
 
-    public static void main(String[] args) {
-        
-    }
+  /**
+   * main.
+   *
+   * @param  args  String[]
+   */
+  public static void main1(String[] args) {
+    System.out.println();
+  }
 }
