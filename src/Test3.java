8d70a352b123b317d65c1066c68db8def0eabf41
Aaron Wang <aaron.wang@hrx.ai>
2018-06-21 13:59:52 +0800

    no message

--- src/Test3.java
+++ src/Test3.java
@@ -4,15 +4,4 @@
  * @author   <a href="mailto:aaron.wang@hrx.ai">Aaron Wang</a>
  * @version  06/21/2018 13:39
  */
-public class Test3 {
-  //~ Methods ----------------------------------------------------------------------------------------------------------
-
-  /**
-   * main.
-   *
-   * @param  args  String[]
-   */
-  public static void main1(String[] args) {
-    System.out.println();
-  }
-}
+public class Test3 { }
